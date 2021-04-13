6a978e608c9bfb391d531d6c4e755cb4aef43b92
Gustavo Garcia <gusgarcia@Gustavos-iMac.local>
2021-04-13 11:52:13 -0400

    nothing major.

---
+++ bootstrap/cache/28ff0b43619a9d9ae080f37039a26a110b554ce1.php
@@ -0,0 +1,3 @@
+<h1>
+    Page Not Found1
+</h1><?php /**PATH /Users/gusgarcia/Sites/Katana/resources/views/errors/404.blade.php ENDPATH**/ ?>
---
+++ bootstrap/cache/35e33e50847cef4259d8dd423ffe45d95983b101.php
@@ -0,0 +1,3 @@
+<h1>
+    Page Not Found
+</h1><?php /**PATH /Users/gusgarcia/Sites/katana/resources/views/errors/404.blade.php ENDPATH**/ ?>
